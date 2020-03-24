c2ba57999ec999c34164f169fca37f626731ea52
elaine <elainetran.tran@gmail.com>
2020-03-24 00:03:13 -0400

    Hamburger menu

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+
 var amountScrolled = 500;
 
 $(window).scroll(function() {
@@ -28,30 +29,30 @@ $('#hamburger li a').click(function(){
 
 // Fade in on scroll
 
-$(function(){  // $(document).ready shorthand
-  $('.project-box').delay(100).fadeIn('slow');
-});
+// $(function(){  // $(document).ready shorthand
+//   $('.project-box').delay(100).fadeIn('slow');
+// });
 
-$(document).ready(function() {
+// $(document).ready(function() {
     
-    /* Every time the window is scrolled ... */
-    $(window).scroll( function(){
+//     /* Every time the window is scrolled ... */
+//     $(window).scroll( function(){
     
-        /* Check the location of each desired element */
-        $('.hideme').each( function(i){
+//         /* Check the location of each desired element */
+//         $('.hideme').each( function(i){
             
-            var bottom_of_object = $(this).position().top + $(this).outerHeight();
-            var bottom_of_window = $(window).scrollTop() + $(window).height();
+//             var bottom_of_object = $(this).position().top + $(this).outerHeight();
+//             var bottom_of_window = $(window).scrollTop() + $(window).height();
             
-            /* If the object is completely visible in the window, fade it it */
-            if( bottom_of_window > bottom_of_object ){
+//             /* If the object is completely visible in the window, fade it it */
+//             if( bottom_of_window > bottom_of_object ){
                 
-                $(this).animate({'opacity':'1'},1500);
+//                 $(this).animate({'opacity':'1'},1500);
                     
-            }
+//             }
             
-        }); 
+//         }); 
     
-    });
+//     });
     
-});
+// });
